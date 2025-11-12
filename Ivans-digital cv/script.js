5376eea80e981c6207a86cae75330ff339778cc9
Ivan Campbell <ivancampbell@icloud.com>
2025-11-12 15:32:23 +0000

    12-11-25

---
+++ Ivans-digital cv/script.js
@@ -0,0 +1,21 @@
+```javascript 
+
+// Smooth scrolling for navigation links 
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
+
+    anchor.addEventListener('click', function (e) { 
+
+        e.preventDefault(); 
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ 
+
+            behavior: 'smooth' 
+
+        }); 
+
+    }); 
+
+}); 
+
+``` 
